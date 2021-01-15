ddf788154f144328b114600f7c1d892fd4a366b1
imjhonny <ramjonathan@gmail.com>
2021-01-15 13:59:01 +0100

    local data test

--- front-end/src/utils/dataFetch.ts
+++ front-end/src/utils/dataFetch.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const CORS_URL = "https://cors-anywhere.herokuapp.com/";
-const BASE_URL = "http://20.71.61.162:8000/project/";
+const BASE_URL = "http://localhost:8000/project/";
 const nAxios = axios.create({
     baseURL: `${BASE_URL}`,
     timeout: 0,
@@ -11,5 +11,6 @@ const nAxios = axios.create({
 
 export const getData = (user: string, project: string) => {
     const url = `${user}/${project}`
+    console.log(url)
     return nAxios.get(url)
 }
